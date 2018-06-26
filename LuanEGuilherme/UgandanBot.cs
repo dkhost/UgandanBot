fdde8f9b9c92f2a8276e565af51a03e1944800e3
@dkhost <37126556+dkhost@users.noreply.github.com>
2018-06-26 18:54:36 -0300

    teste
    teste

---
+++ LuanEGuilherme/UgandanBot.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Drawing;
+using Robocode;
+using Robocode.Util;
+
+namespace LuanEGuilherme
+{
+
+    public class UgandanBot : Robot
+    {
+        private bool peek;
+        private double moveAmount;
+
+        
+        public override void Run()
+        {
+
+            BodyColor = (Color.Black);
+            GunColor = (Color.Orange);
+            RadarColor = (Color.DarkGreen);
+            BulletColor = (Color.PaleGreen);
+            ScanColor = (Color.Black);
+
+
+            moveAmount = Math.Max(855, 595);
+
+            peek = false;
+
+
+            TurnRight(Heading % 90);
+            Ahead(moveAmount);
+
+            peek = true;
+            TurnGunLeft(90);
+            TurnLeft(90);
+
+            while (true)
+            {
+
+                peek = true;
+                Ahead(moveAmount);
+                peek = false;
+                TurnLeft(90);
+            }
+        }
+        public override void OnHitRobot(HitRobotEvent e)
+        {
+
+            if (e.Bearing > -90 && e.Bearing < 90)
+            {
+                Ahead(90);
+            }
+            else
+            {
+                Back(90);
+            }
+        }
+        public override void OnScannedRobot(ScannedRobotEvent e)
+        {
+            Fire(1);
+            if (peek)
+            {
+                Scan();
+            }
+        }
+    }
+}
